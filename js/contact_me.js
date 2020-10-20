541fedaa12294d837d69316635db06aef5ab4973
hiplii-dsw <50976907+hiplii-dsw@users.noreply.github.com>
2020-10-20 22:36:12 +0700

    a

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://docs.google.com/forms/d/e/1FAIpQLSd6HBEvNdOHmzQPTGWek7EMzvC7ZQmcCtHxRVN3-q8enwyqqQ/formResponse",
                 type: "POST",
                 data: {
                     name: name,
